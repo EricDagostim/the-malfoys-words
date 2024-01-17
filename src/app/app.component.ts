2bb81501d1e71abb3b735c59b8ee91c678ec940d
Éric Nascimento <nascimento3ric@gmail.com>
2024-01-17 15:00:53 -0300

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    template: `  
+    <router-outlet></router-outlet>`,
+    imports: [CommonModule, RouterOutlet]
+})
+export class AppComponent {  
+  
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#E0E5FF',
+        secondary: '#4864FF',
+        secondary_20: '#4863ff20',
+        tertiary: '#F6C039',
+        light_gray: '#B0B4C9',
+      },
+    },
+
+  },
+  plugins: [],
+}
